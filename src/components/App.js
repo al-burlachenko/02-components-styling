ecf63a15286b5972fb4caf94c6dd0b5718968a5a
Alexandr <alexander@burlachenko>
2022-12-18 22:18:47 +0200

    adds Box component and plays with it a little bit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import colorPickerOptions from 'colorPickerOptions.json';
 import Alert from './Alert';
+import Box from './Box';
 import ColorPicker from './ColorPicker';
 import Container from './Container';
 
@@ -10,6 +11,10 @@ function App() {
       <Alert text="блблбл, оу фак" type="error" />
       <Alert text="блблбл, оу щит" type="warning" />
       <Alert text="блблбл, оу крэп" type="success" />
+
+      <Box type="small" classNames="big red" />
+      <Box type="medium" />
+      <Box type="large" />
     </Container>
   );
 }
